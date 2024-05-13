756c340ba29fbc94aa7291ad1006afdadad1e8c9
frmatias <vandembergneto@gmail.com>
2024-05-12 21:55:42 -0300

    first commit

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,2 +1,26 @@
-process.stdin
-.pipe(process.stdout)
+import { Readable, Writable } from 'node:stream';
+
+
+class OneToHundredStream extends Readable {
+    index = 1;
+    _read(){
+        const i = this.index++;
+        if(i > 100){
+            this.push(null);
+        } else {
+            const buf = Buffer.from(String(i))
+            this.push(buf);
+        }
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback){
+        const number = parseInt(chunk.toString());
+        const result = number * 10;
+        console.log(result);
+        callback();
+    }
+}
+
+new OneToHundredStream().pipe(process.stdout);
